21ce98fb29c404a4b3b13b114c06f85f20c624d6
Douglas Gross <douglagr@payoneer.com>
2023-11-15 10:59:24 -0500

    fix bylaws page

--- src/routes/bylaws/+page.svelte
+++ src/routes/bylaws/+page.svelte
@@ -1,3 +1,3 @@
-<div class="bylaws">
+<div>
     <iframe src="https://docs.google.com/document/d/e/2PACX-1vTQ5uADmxOD3xCE0Lz3i4whCVpCYCZ47okmDhtkudJOc1VsDEo-QXc1QrDBfFpVJgH6VbzAvzdbVS7O/pub?embedded=true"></iframe>
 </div>
