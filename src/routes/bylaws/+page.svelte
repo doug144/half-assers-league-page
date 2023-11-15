17ea106d31f3d65b8d81fcf51be4420e882aba08
Douglas Gross <douglagr@payoneer.com>
2023-11-15 11:29:15 -0500

    Fix alignment of iframe

--- src/routes/bylaws/+page.svelte
+++ src/routes/bylaws/+page.svelte
@@ -1,5 +1,6 @@
 <div>
 	<a href="https://docs.google.com/document/d/e/2PACX-1vTQ5uADmxOD3xCE0Lz3i4whCVpCYCZ47okmDhtkudJOc1VsDEo-QXc1QrDBfFpVJgH6VbzAvzdbVS7O/pub">Click to view in Google Docs</a>
 	<br>   
-    <iframe src="https://docs.google.com/document/d/e/2PACX-1vTQ5uADmxOD3xCE0Lz3i4whCVpCYCZ47okmDhtkudJOc1VsDEo-QXc1QrDBfFpVJgH6VbzAvzdbVS7O/pub?embedded=true" style="width:100%;height:100%;"></iframe>
+    <iframe src="https://docs.google.com/document/d/e/2PACX-1vTQ5uADmxOD3xCE0Lz3i4whCVpCYCZ47okmDhtkudJOc1VsDEo-QXc1QrDBfFpVJgH6VbzAvzdbVS7O/pub?embedded=true" 
+            style="position:absolute;width:100%;height:100%;"></iframe>
 </div>
