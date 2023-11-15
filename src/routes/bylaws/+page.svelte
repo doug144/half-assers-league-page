1633a338efda195125b36037f0404aede4d099ca
Douglas Gross <douglagr@payoneer.com>
2023-11-15 11:09:28 -0500

    Add iframe style for bylaws

--- src/routes/bylaws/+page.svelte
+++ src/routes/bylaws/+page.svelte
@@ -1,3 +1,3 @@
 <div>
-    <iframe src="https://docs.google.com/document/d/e/2PACX-1vTQ5uADmxOD3xCE0Lz3i4whCVpCYCZ47okmDhtkudJOc1VsDEo-QXc1QrDBfFpVJgH6VbzAvzdbVS7O/pub?embedded=true"></iframe>
+    <iframe src="https://docs.google.com/document/d/e/2PACX-1vTQ5uADmxOD3xCE0Lz3i4whCVpCYCZ47okmDhtkudJOc1VsDEo-QXc1QrDBfFpVJgH6VbzAvzdbVS7O/pub?embedded=true" style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
 </div>
